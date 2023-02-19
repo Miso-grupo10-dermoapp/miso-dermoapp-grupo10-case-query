93610b397f5fde837ed39132adb6dd1e3e90c56b
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-18 19:03:46 -0500

    adding code

--- src/request_validation_utils.py
+++ src/request_validation_utils.py
@@ -3,17 +3,6 @@ import json
 body_properties = ["case_id", "injury_type", "shape", "number_of_lessions", "distributions", "color"]
 
 
-def validate_body_params(body):
-    try:
-        specialties_body = json.loads(body)
-        for property in body_properties:
-            if not validate_property_exist(property, specialties_body):
-                raise RuntimeError("the property {0} is missing or without value".format(property))
-    except Exception as err:
-        raise RuntimeError("Input request is malformed or missing parameters, details " + str(err))
-    return True
-
-
 def validate_property_exist(property, loaded_body):
     if property in loaded_body:
         if loaded_body[property] is not None:
